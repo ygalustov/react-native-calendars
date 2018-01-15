4f72f6cded2d3343ee5aa0b001677bea0324b8ae
macbook <dmytrokazaev@gmail.com>
2018-01-15 16:29:35 +0200

    Removed marking for current day

--- src/calendar/day/basic/index.js
+++ src/calendar/day/basic/index.js
@@ -82,7 +82,7 @@ class Day extends Component {
       containerStyle.push(this.style.selected);
       dotStyle.push(this.style.selectedDot);
       textStyle.push(this.style.selectedText);
-    } else if (marking.dateColor) {
+    } else if (marking.dateColor && this.props.state !== 'today') {
       textStyle.push({color: marking.dateColor});
     } else if (typeof marking.disabled !== 'undefined' ? marking.disabled : this.props.state === 'disabled') {
       textStyle.push(this.style.disabledText);
